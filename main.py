3299a195b31070d8d051fb9f4f9812e9d3f87859
jdcampeau <jdcgithub@duck.com>
2025-10-15 06:01:44 -0500

    added optional --verbose argument

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
+import sys
 import os
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 
 
 def main():
     load_dotenv()
+
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+
+    if not args:
+        print("Lil' Buddy")
+        print('\nUsage: uv run main.py "your prompt here" [--verbose]')
+        print('Example: uv run main.py "How do I build a calculator app?"')
+        sys.exit(1)
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, verbose)
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
-            model='gemini-2.0-flash-001', contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+            model='gemini-2.0-flash-001', 
+            contents = messages
     )
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
     print(response.text)
 
